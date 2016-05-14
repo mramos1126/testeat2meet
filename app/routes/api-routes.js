a29fc55a52b93970037683221cf9c07bb821e581
Mauricio <Ramos@nbp-108-119.nbp.ruw.rutgers.edu>
2016-05-14 11:02:28 -0400

    mauricio

---
+++ app/routes/api-routes.js
@@ -0,0 +1,74 @@
+// *********************************************************************************
+// api-routes.js - this file offers a set of routes for displaying and saving data to the db
+// *********************************************************************************
+
+// Dependencies
+// =============================================================
+var Person 	= require("../model/info.js"); // Pulls out the Character Model
+
+// Routes
+// =============================================================
+module.exports = function(app){
+
+// 	// Search for Specific Character (or all characters) then provides JSON
+// 	app.get('/submit', function(req, res){
+
+// 		// If the user provides a specific character in the URL...
+// 		if(req.params.characters){
+
+// 			// Then display the JSON for ONLY that character.
+// 			// (Note how we're using the ORM here to run our searches)
+
+// 			//go through character name and find req.params.character then bring the json object
+// 			Character.findAll({
+// 				where: {
+// 					routeName: req.params.characters
+// 				}
+// 			}).then(function(result){
+// 				res.json(result);
+// 			})
+// 		}
+
+// 		// Otherwise...
+// 		else{
+// 			// Otherwise display the data for all of the characters. 
+// 			// (Note how we're using Sequelize here to run our searches)
+// 				Character.findAll({})
+// 					.then(function(result){
+// 						res.json(result);
+// 				})
+// 			};
+
+// 	});
+
+// //login aurthentication
+// app.post('/login', 
+//   passport.authenticate('local', { failureRedirect: '/login' }),
+//   function(req, res) {
+//     res.redirect('/');
+//   });
+
+
+	app.post('/api/signup', function(req, res){
+
+		// Take the request...
+		var person = req.body;
+
+		// Create a routeName 
+		//var routeName = person.name.replace(/\s+/g, '').toLowerCase();
+
+		// Then add the character to the database using sequelize
+		Person.create({
+			firstName: person.firstName,
+			lastName: person.lastName,
+			userName: person.userName,
+			age: person.age,
+			email: person.email,
+			gender:person.gender,
+			food:person.food,
+			location:person.location
+		});
+		
+	})
+
+}
---
+++ app/routes/html-routes.js
@@ -0,0 +1,44 @@
+//home: //app.get(/signup)
+		//app.get(/login)
+		//app.get(/questions)
+
+
+		//app.get(/matches)
+
+var path 		= require('path');
+//var passport=require('../login.passport.js')
+
+
+// Routes
+// =============================================================
+module.exports = function(app){
+
+	// Each of the below routes just handles the HTML page that the user gets sent to.
+	app.get('/', function(req, res){
+		res.sendFile(path.join(__dirname + '/../public/index.html'));
+		
+	});
+
+	app.get('/signup', function(req, res){
+		res.sendFile(path.join(__dirname + '/../public/signup.html'));
+	});
+
+	// app.get('/login', function(req, res){
+	// 	res.sendFile(path.join(__dirname + '/../public/login.html'));
+	// });
+	// app.get('/quesitons', function(req, res){
+	// 	res.sendFile(path.join(__dirname + '/../public/questions.html'));
+	// });
+	// app.get('/matches', function(req, res){
+	// 	res.sendFile(path.join(__dirname + '/../public/matches.html'));
+	// });
+	
+ 
+//sucessfull login will go back to home page
+//failed login will stay on page
+ // app.post('/login',
+ //  passport.authenticate('local', { successRedirect: '/questions.html',
+ //                                   failureRedirect: '/login' }));
+  
+}
+
